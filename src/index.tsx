4762aed5e40b4e46475b9e442b4a2aaf4c33ddca
etasdemir <erentasdemircs@gmail.com>
2022-06-01 21:36:27 +0300

    create sections

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import './index.css';
+import About from './screens/about';
+import Contact from './screens/contact';
+import Home from './screens/home';
+import Work from './screens/work';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <div >
-      <span>TEST</span>
-    </div>
+    <Home />
+    <Work />
+    <About />
+    <Contact />
   </React.StrictMode>
 );
