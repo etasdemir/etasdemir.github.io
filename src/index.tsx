6cc701a20d8d7bd7e12f769b871b308276d8cb84
etasdemir <erentasdemircs@gmail.com>
2022-06-03 20:58:23 +0300

    add home section

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import styled from 'styled-components';
 
 import './index.css';
 import About from './screens/about';
@@ -7,14 +8,20 @@ import Contact from './screens/contact';
 import Home from './screens/home';
 import Work from './screens/work';
 
+const SectionContainer = styled.div`
+  display: block;
+`;
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Home />
-    <Work />
-    <About />
-    <Contact />
+    <SectionContainer>
+      <Home />
+      <Work />
+      <About />
+      <Contact />
+    </SectionContainer>
   </React.StrictMode>
 );
--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,9 +1,54 @@
 import React from 'react';
+import styled from 'styled-components';
 
 function Home() {
   return (
-    <div>Home</div>
+    <HomeContainer>
+      <GreetingText>hi there <GreetingEmoji>&#x2728;</GreetingEmoji></GreetingText>
+      <Title>i&apos;m Eren Tasdemir</Title>
+      <Description>...self-motivated and enthusiastic software developer with a strong background in mobile application development from Turkey.</Description>
+      <ProfileContainer>
+        <ProfilePicture src={require('../../assets/profile.jpg')} />
+      </ProfileContainer>
+    </HomeContainer>
   );
 }
+
+const HomeContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`;
+
+const GreetingText = styled.span`
+  font-size: 2vw;
+`;
+
+const GreetingEmoji = styled.span`
+  font-size: 1.5vw;
+`;
+
+const Title = styled.span`
+  font-size: 5vw;
+  font-weight: bold;
+  margin-top: 6vw;
+`;
+
+const Description = styled.span`
+  font-size: 2vw;
+  width: 70%;
+  margin-top: 3vw;
+`;
+
+const ProfileContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+`;
+
+const ProfilePicture = styled.img`
+  height: 25vw;
+  border-radius: 50%;
+  opacity: 0.6;
+`;
 
 export default Home;
