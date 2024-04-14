f64d912519948ad3bba366ab58c8cc036792b0d8
Eren Tasdemir <erentasdemircs@gmail.com>
2024-04-14 18:38:56 +0300

    update descriptions for Scorp

--- src/shared/Constants.ts
+++ src/shared/Constants.ts
@@ -34,7 +34,7 @@ export const ABOUT: AboutElement[] = [
     title: 'summary',
     type: "simple",
     descriptions: [
-      "Currently, I am a Software Developer at Basarsoft. I graduated with a BSc in Computer Science from Hacettepe University. I wrote my first line of code before I started college in 2017. I have been working professionally since the first day of 2021. I mostly developed native Android (Kotlin, Java) and cross-platform mobile applications (React Native). I enjoy learning recently released technologies and trying them out on small projects, especially if they are in alpha or beta releases. I take joy in solving new problems that come with new technology. When I'm not working, I often read programming articles or newsletters and work on my side projects created to test the latest technologies."
+      "Currently, I am an Android Developer at Scorp. I graduated with a BSc in Computer Science from Hacettepe University. I wrote my first line of code before I started college in 2017. I have been working professionally since January 2021. I mostly developed native Android (Kotlin, Java) and cross-platform mobile applications (React Native). I enjoy learning recently released technologies and trying them out on small projects, especially if they are in alpha or beta releases. I take joy in solving new problems that come with new technology. When I'm not working, I often read programming articles or newsletters and work on my side projects created to test the latest technologies."
     ],
   },
   {
@@ -49,35 +49,33 @@ export const ABOUT: AboutElement[] = [
   },
   {
     title: 'favourite newsletters',
-    subtitle: "They're great for keeping up with all the news and everything that's changing in the industry. Today's newsletters help save time by providing great articles, tips, useful resources, learning more about a topic that interests you, and so on. I'm getting addicted to newsletters now.",
+    subtitle: "Newsletters are great for keeping up with all the news and everything changing in the industry. Today's newsletters help save time by providing great articles, tips, useful resources, learning more about a topic that interests you, and so on. I'm getting addicted to newsletters now.",
     type: "hyperlink",
     descriptions: [
       "Android Weekly",
       "Kotlin Weekly",
       "React Native Newsletter",
       "TLDR",
-      "TLDR Crypto",
+      "The Business Engineer",
       "Hacker Newsletter",
       "SoftwareLeadWeekly",
       "Changelog",
       "Frontend Focus",
-      "Frontend Weekly",
-      "Investing.io",
-      "Github Explore",
+      "ByteByteGo",
+      "The Transcript - Weekly Quotes From Earning Calls",
     ],
     urls: [
       "https://androidweekly.net/",
       "http://kotlinweekly.net/",
       "https://reactnativenewsletter.com/",
       "https://tldr.tech/",
-      "https://tldr.tech/crypto",
+      "https://businessengineer.ai/",
       "https://hackernewsletter.com/",
       "https://softwareleadweekly.com/",
-      "https://changelog.com/weekly",
+      "https://changelog.com/news",
       "https://frontendfoc.us/",
-      "https://frontendweekly.co/",
-      "https://smash.vc/startup-newsletter/",
-      "https://github.com/explore/email"
+      "https://blog.bytebytego.com/subscribe",
+      "https://thetranscript.substack.com/subscribe"
     ]
   },
 ];
